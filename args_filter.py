084646d2aa1c508a86f5aca39d83dcfc9388534f
m1gnus <m1gnus@protonmail.com>
2020-07-19 10:24:36 -0400

    args_filter.py:add wrap_int_filter and updated int_filter

--- args_filter.py
+++ args_filter.py
@@ -3,21 +3,31 @@ Takes two parameters: string,base and return the integer value wich is represent
 If base is None, the string will be converted to int by following the standards representations of
 numbers in base 16,2,8,10.
 """
-def int_filter(string: str, base = 0: int) -> int:
+def int_filter(string: str, base = '0') -> int:
     try:
+        base = int(base)
         if base <= 0:
-            if string[:2] == '0x':
+            if string[:2] == '0x': # hexadecimal
                 return int(string[2:], 16)
-            elif string[:2] == '0b':
+            elif string[:2] == '0b': # binary
                 return int(string[2:], 2)
-            elif string[0] == '0':
+            elif string[0] == '0': # octal
                 return int(string[1:], 8)
             else:
-                return int(string, 10)
+                return int(string, 10) # decimal
         else:
-            if base > 9:
+            if base > 9: # invalid base
                 raise ValueError("cannot use base > 9")
-            return int(string, base)
+            return int(string, base) # valid base
     except ValueError as e:
         print("args_filter.py:int_filter ->",e)
 
+"""
+Takes a string in the format string[:base] and return the corresponding integer
+"""
+def wrap_int_filter(string: str) -> int:
+    parameters = [x for x in string.split(":") if x]
+    if len(parameters) > 1: # there is a base
+        return int_filter(parameters[0], parameters[1])
+    else:
+        return int_filter(parameters[0])
