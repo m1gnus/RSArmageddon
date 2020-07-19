de3fcc3f462992858a21d57447236d1602454911
m1gnus <m1gnus@protonmail.com>
2020-07-19 15:57:52 -0400

    add sage's script which implements general purpose factorization method

---
+++ features/sage_factor.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/sage --python
+
+"""
+general purpose factorization
+"""
+
+from sys import argv
+from subprocess import check_output
+
+from sage.all import factor, Integer
+
+def factorize(n: int) -> None:
+    print("[+] Start Factorization")
+    results = str(factor(n)).split(" * ")
+    print("[+] Factorization complete\n")
+    for fact in results:
+        print("[*] " + fact)
+    print()
+
+if __name__ == "__main__":
+    factorize(Integer(argv[1]))
