5f2027b67633dc02b7c727f0f9a51acedd8f07f5
m1gnus <m1gnus@protonmail.com>
2020-07-20 05:12:41 -0400

    general_features.py: fix path for sage_factor.sage and add path for sage_ecm_factor.sage

--- management/general_features.py
+++ management/general_features.py
@@ -1,6 +1,7 @@
 """
 management of general purpose features
 """
+import sys
 
 from parser.args_filter import *
 from os import system
@@ -8,4 +9,9 @@ from os import system
 def general_features_manager(args: object) -> None:
     if args.tofactor:
         int_input = wrap_int_filter(args.tofactor)
-        system("features/sage_factor.py " + str(int_input))
+        system("features/sage_factor.sage " + str(int_input))
+        sys.exit(0)
+    if args.tofactorwecm:
+        int_input = wrap_int_filter(args.tofactorwecm)
+        system("features/sage_ecm_factor.sage " + str(int_input))
+        sys.exit(0)
