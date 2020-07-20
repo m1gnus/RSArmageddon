a0c7e8b136f9e0f976e27d3643d336fa02cb234b
m1gnus <m1gnus@protonmail.com>
2020-07-20 09:41:32 -0400

    general_features.py:add comments

--- management/general_features.py
+++ management/general_features.py
@@ -8,15 +8,15 @@ from os import system
 
 def general_features_manager(args: object) -> None:
 
-    if args.tofactor:
+    if args.tofactor: # --factor <int>
         int_input = wrap_int_filter(args.tofactor)
         system("features/sage_factor.sage " + str(int_input))
         sys.exit(0)
-    if args.tofactorwecm:
+    if args.tofactorwecm: # --ecm <int>
         int_input = wrap_int_filter(args.tofactorwecm)
         system("features/sage_ecm_factor.sage " + str(int_input))
         sys.exit(0)
-    if args.tofactorwqsieve:
+    if args.tofactorwqsieve: # --qsieve <int>
         int_input = wrap_int_filter(args.tofactorwqsieve)
         system("features/sage_qsieve_factor.sage " + str(int_input))
         sys.exit(0)
