88c0c283d8f992a5da054b1b7a33fc552fba4df7
m1gnus <m1gnus@protonmail.com>
2020-07-21 14:24:34 -0400

    pem_manipulation.py:change condition checking order

--- management/pem_manipulation.py
+++ management/pem_manipulation.py
@@ -10,12 +10,13 @@ def pem_manipulation_manager(args: object) -> None:
     if args.dumpvalues:
         check_required(args.key_path)
         dump_values_from_key(args.key_path)
+    if args.generatekeypair:
+        e = (wrap_int_filter(args.e) if args.e else 65537)
+        generate_keypair(e, args.opub, args.opriv)
+        sys.exit(0)
     if args.cpub:
         check_required(args.n, args.e)
         create_pubkey(wrap_int_filter(args.n), wrap_int_filter(args.e), args.opub)
     if args.cpriv:
         create_privkey(args.n, args.e, args.d, args.p, args.q, args.opriv)
-    if args.generatekeypair:
-        e = (wrap_int_filter(args.e) if args.e else 65537)
-        generate_keypair(e, args.opub, args.opriv)
     
