e8c6eedcbb3fc7b81cb585c0c46a4f9a7da48226
m1gnus <m1gnus@protonmail.com>
2020-07-22 09:52:23 -0400

    hash_sum.py:update sha256_file_checksum by implementing a tip form stackoverflow: https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file

--- misc/hash_sum.py
+++ misc/hash_sum.py
@@ -5,10 +5,15 @@ import sys
 import hashlib
 
 def sha256_file_checksum(file_path: str) -> str:
-    hash_md5 = hashlib.md5()
+    hash_sha256 = hashlib.sha256()
     try:
-        hash_md5.update(open(file_path, "rb").read())
+        """
+        from a tip that i saw here: https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+        """
+        with open(file_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_sha256.update(chunk)
     except OSError as e:
         print("hash_sum.py:sha256_file_checksum ->", e)
         sys.exit(1)
-    return hash_md5.hexdigest()
+    return hash_sha256.hexdigest()
