f02c2047721bb31fb92e8b31991312d1f17886f3
m1gnus <m1gnus@protonmail.com>
2020-07-22 09:47:31 -0400

    hash_sum.py:move checksum from md5 to sha256

--- misc/hash_sum.py
+++ misc/hash_sum.py
@@ -4,11 +4,11 @@ checksums
 import sys
 import hashlib
 
-def md5_file_checksum(file_path: str) -> str:
+def sha256_file_checksum(file_path: str) -> str:
     hash_md5 = hashlib.md5()
     try:
         hash_md5.update(open(file_path, "rb").read())
     except OSError as e:
-        print("hash_sum.py:md5_file_checksum ->", e)
+        print("hash_sum.py:sha256_file_checksum ->", e)
         sys.exit(1)
     return hash_md5.hexdigest()
