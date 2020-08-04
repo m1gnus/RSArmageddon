b4e358f478f3378f55681e77c31a8d4790c8ab9f
m1gnus <m1gnus@protonmail.com>
2020-08-04 11:23:08 -0400

    fix minor bug in raising a ValueError in check_required()

--- parsing/args_filter.py
+++ parsing/args_filter.py
@@ -197,7 +197,7 @@ def check_required(*nargs) -> None:
     try:
         for arg in nargs:
             if not arg:
-                raise ValueError("One of the required arguments is not setted: " + " ".join(list(nargs)))
+                raise ValueError("One of the required arguments is not setted")
     except ValueError as e:
         print("args_filter.py:check_required", e)
         sys.exit(1)
