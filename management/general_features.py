a9a0c53c33ba774aeca756c166f1602b2461233f
m1gnus <m1gnus@protonmail.com>
2020-09-04 04:22:06 -0400

    general_features.py: fix a bug in paths

--- management/general_features.py
+++ management/general_features.py
@@ -9,6 +9,7 @@ from banner import credits, show_attacks, version
 from parsing.args_filter import wrap_int_filter
 
 from misc.signal_handler import *
+from misc.software_path import *
 
 def general_features_manager(args: object) -> None:
     
@@ -16,31 +17,31 @@ def general_features_manager(args: object) -> None:
 
     if args.n_phi: # --factor <int>
         int_input = wrap_int_filter(args.n_phi)
-        p = subprocess.Popen(["features/sage_euler_phi.sage", str(int_input)])
+        p = subprocess.Popen([SOFTWARE_PATH + "/features/sage_euler_phi.sage", str(int_input)])
         pids.append(p.pid)
         p.wait()
         sys.exit(0)
     if args.tofactor: # --factor <int>
         int_input = wrap_int_filter(args.tofactor)
-        p = subprocess.Popen(["features/sage_factor.sage", str(int_input)])
+        p = subprocess.Popen([SOFTWARE_PATH + "/features/sage_factor.sage", str(int_input)])
         pids.append(p.pid)
         p.wait()
         sys.exit(0)
     if args.tofactorwecm: # --ecm <int>
         int_input = wrap_int_filter(args.tofactorwecm)
-        p = subprocess.Popen(["features/sage_ecm_factor.sage", str(int_input)])
+        p = subprocess.Popen([SOFTWARE_PATH + "/features/sage_ecm_factor.sage", str(int_input)])
         pids.append(p.pid)
         p.wait()
         sys.exit(0)
     if args.tofactorwqsieve: # --qsieve <int>
         int_input = wrap_int_filter(args.tofactorwqsieve)
-        p = subprocess.Popen(["features/sage_qsieve_factor.sage", str(int_input)])
+        p = subprocess.Popen([SOFTWARE_PATH + "/features/sage_qsieve_factor.sage", str(int_input)])
         pids.append(p.pid)
         p.wait()
         sys.exit(0)
     if args.checkprime: # --isprime <int>
         int_input = wrap_int_filter(args.checkprime)
-        p = subprocess.Popen(["features/sage_isprime.sage", str(int_input)])
+        p = subprocess.Popen([SOFTWARE_PATH + "/features/sage_isprime.sage", str(int_input)])
         pids.append(p.pid)
         p.wait()
         sys.exit(0)
