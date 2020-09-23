72f8b7eb757c03c30eac6e63f74d83aeb21b3ea2
m1gnus <m1gnus@protonmail.com>
2020-09-23 17:44:26 +0200

    Refactor application core

--- RSAGD_core.py
+++ RSAGD_core.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/sage --python
+#!/usr/bin/env python3
 
 from banner import banner
 
-from parsing.parser_config import *
+from args import args
 
 from management.general_features import *
 from management.pem_manipulation import *
@@ -11,15 +11,19 @@ from management.attacks_manager import *
 
 from misc.signal_handler import *
 
-if __name__ == "__main__":
 
+def main():
     banner()
 
-    if not args.subp:
-        general_features_manager(args)
-    elif args.subp == "pem":
-        pem_manipulation_manager(args)
-    elif args.subp == "ciphertool":
-        ciphertool_manager(args)
-    elif args.subp == "attack":
-        attack_manager(args)
+    actions = {
+        "": general_features_manager,
+        "pem": pem_manipulation_manager,
+        "ciphertool": ciphertool_manager,
+        "attack": attack_manager
+    }
+
+    actions[args.subp]()
+
+
+if __name__ == "__main__":
+    main()
