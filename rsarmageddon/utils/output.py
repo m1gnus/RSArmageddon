2b90abe44dfcda723ed6a4a72286ab543fc5fbed
Simone Cimarelli <aquilairreale@ymail.com>
2021-03-08 01:01:30 +0100

    Improve multiline output support

--- rsarmageddon/utils/output.py
+++ rsarmageddon/utils/output.py
@@ -23,10 +23,6 @@ import colorama
 from colorama import Fore
 
 
-def _print(*args, **kwargs):
-    print(*args, **kwargs, file=sys.stderr, flush=True)
-
-
 def init(color="auto"):
     if color == "auto":
         colorama.init()
@@ -38,43 +34,60 @@ def init(color="auto"):
         raise ValueError(f"Bad color setting {color!r}")
 
 
-def yellow(msg, newline=True):
-    _print(f"{Fore.YELLOW}{msg}{Fore.RESET}", end=("\n" if newline else ""))
-
-
-def white(msg, newline=True):
-    _print(f"{Fore.LIGHTWHITE_EX}{msg}{Fore.RESET}", end=("\n" if newline else ""))
-
-
-def success(msg, newline=True):
-    _print(f"[{Fore.GREEN}+{Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
-def primary(msg, newline=True):
-    _print(f"[{Fore.CYAN}*{Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
-def secondary(msg, newline=True):
-    _print(f"[{Fore.MAGENTA}#{Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
-def info(msg, newline=True):
-    _print(f"[{Fore.BLUE}${Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
-def warning(msg, newline=True):
-    _print(f"[{Fore.LIGHTYELLOW_EX}W{Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
-def error(msg, newline=True):
-    _print(f"[{Fore.RED}-{Fore.RESET}] {msg}", end=("\n" if newline else ""))
-
-
 def newline():
-    _print()
+    print(file=sys.stderr)
+
+
+def _print(*args, **kwargs):
+    print(*args, **kwargs, end="", file=sys.stderr)
+
+
+def make_output(name, color=Fore.RESET, widget=None, widget_color=None):
+    if widget_color is None:
+        widget_color = ""
+
+    widget_text = ""
+    indent = 0
+    if widget is not None:
+        widget_text = f"[{widget_color}{widget}{Fore.RESET}] "
+        indent = len(widget) + 3
+
+    def output(msg, newline=True):
+        msg = str(msg)
+        lines = iter(msg.splitlines() or [""])
+
+        firstline = next(lines)
+        _print(widget_text)
+        _print(f"{color}{firstline}")
+
+        for line in lines:
+            _print("\n")
+            _print(" " * indent)
+            _print(f"{line}")
+
+        _print(Fore.RESET)
+
+        if newline:
+            _print("\n")
+
+        sys.stderr.flush()
+
+    output.__name__ = name
+    return output
+
+
+yellow = make_output("yellow", Fore.YELLOW)
+white = make_output("white", Fore.LIGHTWHITE_EX)
+success = make_output("success", widget="+", widget_color=Fore.GREEN)
+primary = make_output("primary", widget="*", widget_color=Fore.CYAN)
+secondary = make_output("secondary", widget="#", widget_color=Fore.MAGENTA)
+info = make_output("info", widget="$", widget_color=Fore.BLUE)
+warning = make_output("warning", widget="W", widget_color=Fore.LIGHTYELLOW_EX)
+error = make_output("error", widget="-", widget_color=Fore.RED)
 
 
 if __name__ == "__main__":
+    init(color="always")
     yellow("Yellow text")
     white("White text")
     success("Success")
@@ -83,3 +96,12 @@ if __name__ == "__main__":
     info("Info")
     warning("Warning")
     error("Error")
+    newline()
+    yellow("Multiline\nyellow")
+    white("Multiline\nwhite")
+    success("Multiline\nsuccess")
+    primary("Multiline\nprimary output")
+    secondary("Multiline\nsecondary output")
+    info("Multiline\ninfo")
+    warning("Multiline\nwarning")
+    error("Multiline\nerror")
