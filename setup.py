9567525050c346c1d7843d777ad6a3e7b041d609
kaisersource <emanuelebosimini@gmail.com>
2023-12-30 00:18:14 +0100

    shlex escape + try catch

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from pathlib import Path
 from setuptools import setup
+import shlex
 
-def sh_escape(f):
-    f = str(f).replace("'", r"'\''")
-    return f"'{f}'"
+def sh_escape(file_path):
+    """Shell-escapes the file path."""
+    return shlex.quote(str(file_path))
 
-print("cleaning up compiled sage files")
-for f in Path("rsarmageddon").glob("**/*.sage.py"):
-    if not f.is_dir():
-        f.unlink(missing_ok=True)
-        print(f"removed {sh_escape(f)}")
+def clean_sage_files(directory: Path):
+    """Removes compiled Sage files from the specified directory."""
+    for file_path in directory.glob("**/*.sage.py"):
+        if file_path.is_file():
+            try:
+                file_path.unlink(missing_ok=True)
+                print(f"Removed {sh_escape(file_path)}")
+            except OSError as e:
+                print(f"Error removing {sh_escape(file_path)}: {e}")
+
+print("Cleaning up compiled Sage files...")
+clean_sage_files(Path("rsarmageddon"))
 
 setup()
